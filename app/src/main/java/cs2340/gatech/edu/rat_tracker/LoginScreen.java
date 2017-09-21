03d4648d347017d424da2c350320082b84adf23a
unknown <dallas.downing@comcast.net>
2017-09-21 17:38:28 -0400

    Created Android Project

---
+++ app/src/main/java/cs2340/gatech/edu/rat_tracker/LoginScreen.java
@@ -0,0 +1,13 @@
+package cs2340.gatech.edu.rat_tracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
