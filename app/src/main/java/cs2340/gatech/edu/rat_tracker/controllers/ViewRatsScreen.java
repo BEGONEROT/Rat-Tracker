f23aa79e1865f04e8ef9611846320700100704a5
Aadarsh Padiyath <aspadiyath@gmail.com>
2017-10-05 18:13:21 -0400

    ViewRats Screen and WelcomeScreen

---
+++ app/src/main/java/cs2340/gatech/edu/rat_tracker/controllers/ViewRatsScreen.java
@@ -0,0 +1,31 @@
+package cs2340.gatech.edu.rat_tracker.controllers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import cs2340.gatech.edu.rat_tracker.R;
+
+/**
+ * Created by Aadarsh on 10/5/2017.
+ */
+
+public class ViewRatsScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_rats_screen);
+    }
+
+    /**
+     * Changes the activity to start screen when logout is pressed
+     * @param v Current view
+     */
+    public void onViewListPressed(View v) {
+        Intent logOutPage = new Intent(ViewRatsScreen.this, RatData.class);
+        startActivity(logOutPage);
+    }
+
+
+}
