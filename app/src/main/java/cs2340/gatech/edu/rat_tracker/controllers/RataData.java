acc91076f727b9398ba941405cd0faa909199d61
david-w-key <dwarrenkey27@gmail.com>
2017-10-11 14:54:53 -0400

    Trying to work out the ListView functionality

--- app/src/main/java/cs2340/gatech/edu/rat_tracker/controllers/RataData.java
+++ app/src/main/java/cs2340/gatech/edu/rat_tracker/controllers/RataData.java
@@ -4,10 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.content.Intent;
 
+import java.io.IOException;
+import java.util.HashMap;
+
 import cs2340.gatech.edu.rat_tracker.R;
+import cs2340.gatech.edu.rat_tracker.model.Model;
+import  cs2340.gatech.edu.rat_tracker.model.RatData;
 
 public class RataData extends ListActivity {
 
@@ -15,6 +21,15 @@ public class RataData extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rata_data);
+        ListView ratdataview = (ListView) findViewById(R.id.RataData);
+        try {
+            Model.getInstance().readRatData();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        HashMap<Integer, RatData> sightings = (HashMap<Integer, RatData>) Model.getInstance().getAllRatData();
+        ratdataview.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sightings));
     }
 
     @Override
@@ -22,6 +37,22 @@ public class RataData extends ListActivity {
         Intent intent = new Intent(getBaseContext(), RatDetails.class);
         intent.putExtra("SIGHTING", ratSighting);
         startActivity(intent);
+    }
+
+    /*
+     * This method refreshes the information in the ListView
+     *
+     * @param v the view
+     */
+    public void onRefreshPressed(View v) {
+        ListView ratdataview = (ListView) findViewById(R.id.RataData);
+        try {
+            Model.getInstance().readRatData();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        HashMap<Integer, RatData> sightings = (HashMap<Integer, RatData>) Model.getInstance().getAllRatData();
+        ratdataview.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sightings));
     }
     
 }
