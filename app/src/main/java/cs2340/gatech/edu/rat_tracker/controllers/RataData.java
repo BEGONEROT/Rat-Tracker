b4c4d55dbf2e35f83769d558bbeeadabc30d5d2a
david-w-key <dwarrenkey27@gmail.com>
2017-10-11 11:58:44 -0400

    Adding the ability to click on a ListView item and go to rat details

--- app/src/main/java/cs2340/gatech/edu/rat_tracker/controllers/RataData.java
+++ app/src/main/java/cs2340/gatech/edu/rat_tracker/controllers/RataData.java
@@ -1,16 +1,27 @@
 package cs2340.gatech.edu.rat_tracker.controllers;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.content.Intent;
 
 import cs2340.gatech.edu.rat_tracker.R;
 
-public class RataData extends AppCompatActivity {
+public class RataData extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rata_data);
+    }
+
+    @Override
+    public void onListItemClick(ListView item, View v, int position, long id)  {
+        Intent intent = new Intent(getBaseContext(), RatDetails.class);
+        intent.putExtra("SIGHTING", ratSighting);
+        startActivity(intent);
     }
     
 }
