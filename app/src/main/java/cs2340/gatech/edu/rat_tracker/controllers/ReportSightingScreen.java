22c4d2cbc6656c389959a2ba3c07e23315a7e081
Davon Prewitt <davonprewitt@lawn-128-61-63-200.lawn.gatech.edu>
2017-10-26 13:45:24 -0400

    Merging with master

--- app/src/main/java/cs2340/gatech/edu/rat_tracker/controllers/ReportSightingScreen.java
+++ app/src/main/java/cs2340/gatech/edu/rat_tracker/controllers/ReportSightingScreen.java
@@ -6,10 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-<<<<<<< HEAD
-import android.widget.Spinner;
-import android.widget.TextView;
-=======
+
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -18,19 +15,19 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 
->>>>>>> origin/master
+
 
 import org.w3c.dom.Text;
 
 import cs2340.gatech.edu.rat_tracker.R;
-<<<<<<< HEAD
+
 import cs2340.gatech.edu.rat_tracker.model.RatSighting;
-=======
+
 import cs2340.gatech.edu.rat_tracker.model.Borough;
 import cs2340.gatech.edu.rat_tracker.model.LocationType;
 import cs2340.gatech.edu.rat_tracker.model.Model;
 
->>>>>>> origin/master
+
 
 /**
  * Created by davonprewitt on 10/15/17.
@@ -48,40 +45,7 @@ public class ReportSightingScreen extends AppCompatActivity {
         setContentView(R.layout.activity_new_sighting_screen);
         final Button submit = (Button) findViewById(R.id.submit);
 
-<<<<<<< HEAD
 
-
-
-
-
-        Spinner residenceTypeSpinner = (Spinner) findViewById(R.id.residenceTypeSpinner);
-
-
-        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, RatSighting.resTypes);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        residenceTypeSpinner.setAdapter(adapter);
-
-
-        TextView cityField = (TextView) findViewById(R.id.cityText);
-
-
-        TextView boroughField = (TextView) findViewById(R.id.boroughText);
-
-
-
-        TextView addressField = (TextView) findViewById(R.id.address);
-
-
-        submit.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String residenceType = (String) residenceTypeSpinner.getSelectedItem();
-                String city = (String) cityField.getText().toString();
-                String borough = (String) boroughField.getText().toString();
-                String address = addressField.getText().toString();
-
-
-
-=======
         EditText city = (EditText) findViewById(R.id.cityText);
 
         Spinner borough = (Spinner) findViewById(R.id.boroughSpinner);
@@ -122,7 +86,7 @@ public class ReportSightingScreen extends AppCompatActivity {
 
                 //TESTING
                 //updateDatabase("home", "bronx", "ny", "today", "6 blah way", "45701", 41.402, -23.401, "farm");
->>>>>>> origin/master
+
 
 
             }
