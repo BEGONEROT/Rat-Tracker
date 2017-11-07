7e95a7d51b69bdaba3546831692759cdd847c121
Aadarsh Padiyath <aspadiyath@gmail.com>
2017-11-06 20:50:40 -0500

    Added implementation for settings

---
+++ app/src/main/java/cs2340/gatech/edu/rat_tracker/controllers/UpdatePasswordScreen.java
@@ -0,0 +1,51 @@
+package cs2340.gatech.edu.rat_tracker.controllers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
+import cs2340.gatech.edu.rat_tracker.R;
+
+/**
+ * Created by Aadarsh on 11/6/2017.
+ */
+
+public class UpdatePasswordScreen extends AppCompatActivity {
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+
+    /**
+     * Changes the activity
+     * @param v Current view
+     */
+    public void onUpdatePasswordPressed(View v) {
+        EditText oldPass = (EditText) findViewById(R.id.oldPassword);
+        String oldPassword = oldPass.getText().toString();
+        EditText newPass = (EditText) findViewById(R.id.newPass);
+        String newPassword = newPass.getText().toString();
+        EditText confirmNewPass = (EditText) findViewById(R.id.confirmNewPass);
+        String confirmNewPassWord = confirmNewPass.getText().toString();
+
+        if (!Objects.equals(oldPassword, "") && Objects.equals(oldPassword, newPassword)) {
+            if (newPassword.equals(confirmNewPassWord) && !Objects.equals(newPassword, "")) {
+                mAuth.getCurrentUser().updatePassword(newPassword);
+            }
+        }
+
+        Intent logOutPage = new Intent(UpdatePasswordScreen.this, SettingsScreen.class);
+        startActivity(logOutPage);
+    }
+
+}
---
+++ app/src/main/java/cs2340/gatech/edu/rat_tracker/controllers/updateEmailScreen.java
@@ -0,0 +1,47 @@
+package cs2340.gatech.edu.rat_tracker.controllers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import cs2340.gatech.edu.rat_tracker.R;
+
+/**
+ * Created by Aadarsh on 11/6/2017.
+ */
+
+public class updateEmailScreen extends AppCompatActivity {
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_email);
+
+        String oldEmail = mAuth.getCurrentUser().getEmail();
+
+        TextView email = (TextView) findViewById(R.id.oldEmail);
+        email.setText(oldEmail);
+    }
+
+    /**
+     * Changes the activity
+     * @param v Current view
+     */
+    public void onUpdateEmailPressed(View v) {
+        EditText usernameField = (EditText) findViewById(R.id.newEmail);
+        String username = usernameField.getText().toString();
+        mAuth.getCurrentUser().updateEmail(username);
+
+
+        Intent logOutPage = new Intent(updateEmailScreen.this, SettingsScreen.class);
+        startActivity(logOutPage);
+    }
+
+}
